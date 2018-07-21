9be2097417705a7944e17744ce69c4f70733f1cb
Shawon Ashraf <shawon13@live.com>
2018-07-21 14:29:24 +0600

    updated url

--- Clima/Clima/AppConstants.swift
+++ Clima/Clima/AppConstants.swift
@@ -9,6 +9,6 @@
 import Foundation
 
 class AppConstants {
-    static let WEATHER_URL = "api.openweathermap.org/data/2.5/weather"
+    static let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
     static let APP_ID =  "c734f0ee9576510e56720c7f5e0003ab"
 }
