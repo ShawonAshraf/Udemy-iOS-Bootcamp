9f8dd4df6705d9ad30a0687b86415b5299f780d7
Shawon Ashraf <shawon13@live.com>
2018-07-21 16:37:36 +0600

    segue example

---
+++ SegueExample/SegueExample/SecondVC.swift
@@ -0,0 +1,22 @@
+//
+//  SecondVC.swift
+//  SegueExample
+//
+//  Created by Shawon Ashraf on 7/21/18.
+//  Copyright © 2018 Shawon Ashraf. All rights reserved.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+    @IBOutlet var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ SegueExample/SegueExample/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SegueExample
+//
+//  Created by Shawon Ashraf on 7/21/18.
+//  Copyright © 2018 Shawon Ashraf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var testField: UITextField!
+    @IBOutlet var btn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func navToSecondVC(_ sender: Any) {
+    }
+    
+}
+
