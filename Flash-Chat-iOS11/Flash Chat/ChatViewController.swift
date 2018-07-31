1f5054146be2245cc47adcadd7572ba814f8acb8
Shawon Ashraf <shawon13@live.com>
2018-08-01 03:23:00 +0600

    flash chat done

--- Flash-Chat-iOS11/Flash Chat/ChatViewController.swift
+++ Flash-Chat-iOS11/Flash Chat/ChatViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import Firebase
+import SVProgressHUD
+import ChameleonFramework
 
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+    
     
     // Declare instance variables here
+    var messageArray: [Message] = []
 
     
     // We've pre-linked the IBOutlets
@@ -26,19 +31,26 @@ class ChatViewController: UIViewController {
         super.viewDidLoad()
         
         //TODO: Set yourself as the delegate and datasource here:
-        
-        
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
+        messageTextfield.delegate = self
         
         //TODO: Set yourself as the delegate of the text field here:
 
         
         
         //TODO: Set the tapGesture here:
-        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tableViewTapped))
+        messageTableView.addGestureRecognizer(tapGesture)
         
 
         //TODO: Register your MessageCell.xib file here:
-
+        messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
+        
+        configureTableView()
+        retrieveMessages()
+        
+        messageTableView.separatorStyle = .none
         
     }
 
@@ -47,20 +59,46 @@ class ChatViewController: UIViewController {
     //MARK: - TableView DataSource Methods
     
     
-    
     //TODO: Declare cellForRowAtIndexPath here:
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
+        
+        cell.messageBody.text = self.messageArray[indexPath.row].messageBody
+        cell.senderUsername.text = self.messageArray[indexPath.row].sender
+        cell.avatarImageView.image = #imageLiteral(resourceName: "egg")
+        
+        if cell.senderUsername.text == Auth.auth().currentUser?.email as String! {
+            
+            cell.avatarImageView.backgroundColor = UIColor.flatMint()
+            cell.messageBackground.backgroundColor = UIColor.flatSkyBlue()
+        } else {
+            cell.avatarImageView.backgroundColor = UIColor.flatWatermelon()
+            cell.messageBackground.backgroundColor = UIColor.flatGray()
+        }
+        
+        return cell
+    }
     
     
     
     //TODO: Declare numberOfRowsInSection here:
-    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.messageArray.count
+    }
     
     
     //TODO: Declare tableViewTapped here:
+    @objc func tableViewTapped() {
+        messageTextfield.endEditing(true)
+    }
     
     
     
     //TODO: Declare configureTableView here:
+    func configureTableView() {
+        messageTableView.rowHeight = UITableViewAutomaticDimension
+        messageTableView.estimatedRowHeight = 120.0
+    }
     
     
     
@@ -72,12 +110,22 @@ class ChatViewController: UIViewController {
 
     
     //TODO: Declare textFieldDidBeginEditing here:
-    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 308
+            self.view.layoutIfNeeded()
+        }
+    }
     
     
     
     //TODO: Declare textFieldDidEndEditing here:
-    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 50
+            self.view.layoutIfNeeded()
+        }
+    }
 
     
     ///////////////////////////////////////////
@@ -91,13 +139,53 @@ class ChatViewController: UIViewController {
     
     @IBAction func sendPressed(_ sender: AnyObject) {
         
+        messageTextfield.endEditing(false)
         
         //TODO: Send the message to Firebase and save it in our database
         
+        messageTextfield.isEnabled = false
+        sendButton.isEnabled = false
         
+        let messagesDB = Database.database().reference().child("Messages")
+        let messageDictionary = ["Sender": Auth.auth().currentUser?.email!, "MessageBody": messageTextfield.text!]
+        
+        messagesDB.childByAutoId().setValue(messageDictionary) {
+            (error, reference) in
+            
+            if error != nil {
+                print(error)
+            } else {
+                print("Message saved successfully")
+                self.messageTextfield.isEnabled = true
+                self.sendButton.isEnabled = true
+                self.messageTextfield.text = ""
+            }
+        }
     }
     
     //TODO: Create the retrieveMessages method here:
+    func retrieveMessages() {
+        // ProgressHUD
+        SVProgressHUD.show()
+        
+        let messageDB = Database.database().reference().child("Messages")
+        messageDB.observe(.childAdded) { (snapshot) in
+            let snapshotValue = snapshot.value as! Dictionary<String, String>
+            
+            let text = snapshotValue["MessageBody"]!
+            let sender = snapshotValue["Sender"]!
+            
+            
+            // create the message object
+            let message = Message(sender: sender, messageBody: text)
+            self.messageArray.append(message)
+            
+            self.configureTableView()
+            self.messageTableView.reloadData()
+            
+            SVProgressHUD.dismiss()
+        }
+    }
     
     
 
@@ -107,7 +195,15 @@ class ChatViewController: UIViewController {
     @IBAction func logOutPressed(_ sender: AnyObject) {
         
         //TODO: Log out the user and send them back to WelcomeViewController
-        
+        do {
+            try Auth.auth().signOut()
+            guard (navigationController?.popToRootViewController(animated: true)) != nil else {
+                print("No view controllers to pop off")
+                return
+            }
+        } catch {
+            print("Can't log user out!")
+        }
         
     }
     
--- Flash-Chat-iOS11/Flash Chat/Message.swift
+++ Flash-Chat-iOS11/Flash Chat/Message.swift
@@ -8,6 +8,18 @@ class Message {
     
     //TODO: Messages need a messageBody and a sender variable
     
+    var sender: String
+    var messageBody: String
+    
+    init() {
+        self.messageBody = ""
+        self.sender = ""
+    }
+    
+    init(sender: String, messageBody: String) {
+        self.messageBody = messageBody
+        self.sender = sender
+    }
     
     
 }
