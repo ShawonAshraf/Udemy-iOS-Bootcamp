16b71e2e49df9171d5446a3d22c39f5c6f29cb91
Shawon Ashraf <shawon13@live.com>
2018-08-03 04:53:19 +0600

    user defaults, item class, groups

--- Todoey/Todoey/Controller/TodoListViewController.swift
+++ Todoey/Todoey/Controller/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Find Mike", "Buy eggs", "Pay bills", "Destroy Demogorgon"]
+    var itemArray: [Item] = [Item]()
     let defaults = UserDefaults.standard
     
     @IBOutlet weak var addButton: UIBarButtonItem!
@@ -18,6 +18,23 @@ class TodoListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let newItem = Item()
+        newItem.title = "Watch Sword Gai again"
+        newItem.done = false
+        itemArray.append(newItem)
+        
+        let anotherNewItem = Item()
+        anotherNewItem.title = "Stay away from facebook"
+        anotherNewItem.done = false
+        itemArray.append(anotherNewItem)
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+            itemArray = items
+        } else {
+            print("No default storage data found.")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,8 +48,13 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (_) in
             if let text = textField.text {
-                self.itemArray.append(text)
+                let newItem = Item()
+                newItem.title = text
+
+                self.itemArray.append(newItem)
+                
                 self.defaults.set(self.itemArray, forKey: "TodoListArray")
+                
                 self.tableView.reloadData()
             } else {
                 // GG
@@ -56,18 +78,23 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        cell.accessoryType = itemArray[indexPath.row].done ? .checkmark : .none
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
     
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        } else {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
